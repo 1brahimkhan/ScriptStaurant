5a21054a75a890293abb8927e4f5d515ca4bf89b
Khan Ibrahim <md.ibrahim.khan98@gmail.com>
2022-06-10 11:39:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Navbar } from "./MyComponents/Navbar";
+import { Body } from "./MyComponents/Body";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
